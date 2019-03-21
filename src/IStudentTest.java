b32dbab7ca4005d61e224e51909b5395119dd630
Namnextx <phuongnam.nextx@gmail.com>
2019-03-21 09:58:38 +0700

    Done ver1.0

--- src/IStudentTest.java
+++ src/IStudentTest.java
@@ -1,18 +1,13 @@
 public interface IStudentTest {
 
      void showListStudent() throws Exception;
-
-
      void addNewStudent() throws Exception;
      void sortStudentByID()throws Exception;
      void reNameStudentAddIndex() throws Exception;
      void removeStudentAddIndex(String idOfStudent) throws Exception;
      Student findStudentSumScoreMax() throws Exception;
     /*
-
-    public void findSudentIdStudent();
     public void refactorStudent();
-
     public void checkStudent();
     */
 }
