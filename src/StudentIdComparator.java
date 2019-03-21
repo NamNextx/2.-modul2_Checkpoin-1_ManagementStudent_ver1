3fb5fc256bad5cecd88259042acb993560e017e8
Namnextx <phuongnam.nextx@gmail.com>
2019-03-21 16:16:51 +0700

    update: change type of idStudent from String to double type

--- src/StudentIdComparator.java
+++ src/StudentIdComparator.java
@@ -4,8 +4,9 @@ public class StudentIdComparator implements Comparator<Student> {
 
     @Override
     public int compare(Student student1, Student student2) {
-        int number1=Integer.parseInt(student1.getIdStudent());
-        int number2=Integer.parseInt(student2.getIdStudent());
+
+        int number1 = (int) student1.getIdStudent();
+        int number2 = (int) student2.getIdStudent();
 
         if (number1 > number2) {
             return 1;
